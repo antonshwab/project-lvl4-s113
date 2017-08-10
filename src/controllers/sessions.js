9f3d3e4239c4e1297ff036085b3f5e62c6f1d231
Anton Shvab <shwab.ay@gmail.com>
2017-08-10 17:40:48 +0300

    add logger

--- src/controllers/sessions.js
+++ src/controllers/sessions.js
@@ -1,5 +1,8 @@
 import buildFormObj from '../lib/formObjectBuilder';
 import { encrypt } from '../lib/secure';
+import logger from '../lib/logger';
+
+const log = logger('app:session');
 
 export default (router, { User }) => {
   router
@@ -9,17 +12,18 @@ export default (router, { User }) => {
     })
     .post('session', '/session', async (ctx) => {
       const { email, password } = ctx.request.body.form;
+      log('Sign in with email: %s, password: %s', email, password);
       const user = await User.findOne({
         where: {
           email,
         },
       });
+      log('User: %o', user);
       if (user && user.passwordDigest === encrypt(password)) {
         ctx.session.userId = user.id;
         ctx.redirect(router.url('root'));
         return;
       }
-
       ctx.flash.set('email or password were wrong');
       ctx.render('sessions/new', { f: buildFormObj({ email }) });
     })
