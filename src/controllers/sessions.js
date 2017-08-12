91a66896cd006e14432dc05a987aa7cc70ffd20f
Anton Shvab <shwab.ay@gmail.com>
2017-08-12 15:50:59 +0300

    Add logs ans flashes

--- src/controllers/sessions.js
+++ src/controllers/sessions.js
@@ -11,6 +11,7 @@ export default (router, { User }) => {
       ctx.render('sessions/new', { f: buildFormObj(data) });
     })
     .post('session', '/session', async (ctx) => {
+      log('Sign in form: %o', ctx.request.body.form);
       const { email, password } = ctx.request.body.form;
       log('Sign in with email: %s, password: %s', email, password);
       const user = await User.findOne({
@@ -21,14 +22,21 @@ export default (router, { User }) => {
       log('User: %o', user);
       if (user && user.passwordDigest === encrypt(password)) {
         ctx.session.userId = user.id;
+        ctx.flash.set('You singed in');
         ctx.redirect(router.url('root'));
         return;
       }
+      log('email or password were wrong or user don\'t exist');
       ctx.flash.set('email or password were wrong');
+      ctx.state.flashNowMsg = 'email or password were wrong';
+      log('current ctx.state: %o', ctx.state);
+
       ctx.render('sessions/new', { f: buildFormObj({ email }) });
     })
     .delete('session', '/session', (ctx) => {
+      log('Current session object: %o', ctx.session);
       ctx.session = {};
+      ctx.flash.set('You signed out');
       ctx.redirect(router.url('root'));
     });
 };
