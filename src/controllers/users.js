a01f45814dd87fff7fe5eadcdab4f0db3f71bb67
Anton Shvab <shwab.ay@gmail.com>
2017-08-12 15:53:41 +0300

    Add CRUD permissions

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,5 +1,8 @@
 import rollbar from 'rollbar';
 import buildFormObj from '../lib/formObjectBuilder';
+import logger from '../lib/logger';
+
+const log = logger('app:users');
 
 export default (router, { User }) => {
   router
@@ -11,8 +14,9 @@ export default (router, { User }) => {
       const user = User.build();
       ctx.render('users/new', { f: buildFormObj(user) });
     })
-    .post('users', '/users', async (ctx) => {
+    .post('newUser', '/users', async (ctx) => {
       const form = ctx.request.body.form;
+      log('form: %o', ctx.request.body.form);
       const user = User.build(form);
       try {
         await user.save();
@@ -23,49 +27,62 @@ export default (router, { User }) => {
         ctx.render('users/new', { f: buildFormObj(user, err) });
       }
     })
-    .get('profile', '/users/:id', async (ctx) => {
+    .get('showUser', '/users/:id', async (ctx) => {
       const userId = Number(ctx.params.id);
       const user = await User.findById(userId);
-      if (user) {
-        ctx.render('users/profile', { user });
+      if (user && userId === ctx.session.userId) {
+        ctx.render('users/show', { user });
       } else {
+        ctx.flash.set('You need sign in to view this profile');
         ctx.redirect(router.url('newSession'));
       }
     })
     .get('editProfile', '/users/:id/edit', async (ctx) => {
       const userId = Number(ctx.params.id);
-      const user = await User.findById(userId);
-      ctx.render('users/editProfile', { f: buildFormObj(user) });
+      if (ctx.session.userId === userId) {
+        const user = await User.findById(userId);
+        ctx.render('users/edit', { f: buildFormObj(user) });
+      } else {
+        ctx.flash.set('You need sign in to edit this profile');
+        ctx.redirect(router.url('newSession'));
+      }
     })
     .patch('updateProfile', '/users/:id', async (ctx) => {
       const userId = Number(ctx.params.id);
       const form = ctx.request.body.form;
       const user = await User.findById(userId);
-      try {
-        await user.update({
-          email: form.email,
-          firstName: form.firstName,
-          lastName: form.lastName,
-          password: form.password,
-        }, { where: { id: userId } });
-        ctx.flash.set('Profile was updated');
-        ctx.redirect(`/users/${userId}`);
-      } catch (err) {
-        rollbar.handleError(err);
-        ctx.render('users/editProfile', { f: buildFormObj(user, err) });
+      if (ctx.session.userId === userId) {
+        try {
+          await user.update({
+            email: form.email,
+            firstName: form.firstName,
+            lastName: form.lastName,
+            password: form.password,
+          }, { where: { id: userId } });
+          ctx.flash.set('User profile was updated');
+          ctx.redirect(`/users/${userId}`);
+        } catch (err) {
+          rollbar.handleError(err);
+          ctx.render('users/edit', { f: buildFormObj(user, err) });
+        }
+      } else {
+        ctx.flash.set('You need sign in to edit this profile');
+        ctx.redirect(router.url('newSession'));
       }
     })
-    .delete('deleteProfile', '/users/:id', async (ctx) => {
+    .delete('deleteUser', '/users/:id', async (ctx) => {
       const userId = Number(ctx.params.id);
       if (ctx.session.userId === userId) {
         await User.destroy({
           where: { id: userId },
         });
+        log('Current session object: %o', ctx.session);
         ctx.session = {};
+        ctx.flash.set('You was deleted');
         ctx.redirect(router.url('root'));
       } else {
-        ctx.flash.set('You can\'t delete other');
-        ctx.redirect(router.url('root'));
+        ctx.flash.set('You need sing in to edit this profile');
+        ctx.redirect(router.url('newSession'));
       }
     });
 };
