8f8869f928b9f80e81fe89bdbc4122a0e0703272
Anton Shvab <shwab.ay@gmail.com>
2017-08-12 15:32:18 +0300

    Add test for users CRUD

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import matchers from 'jest-supertest-matchers';
+import faker from 'faker';
 
 import app from '../src';
 
@@ -24,6 +25,92 @@ describe('requests', () => {
     const res = await request.agent(server)
       .get('/wrong-path');
     expect(res).toHaveHTTPStatus(404);
+  });
+
+  it('sign up', async () => {
+    const res = await request.agent(server)
+      .post('/users')
+      .send({
+        form: {
+          email: faker.internet.email(),
+          firstName: faker.name.firstName(),
+          lastName: faker.name.lastName(),
+          password: faker.internet.password(),
+        },
+      });
+    expect(res).toHaveHTTPStatus(302);
+    expect(res.headers.location).toBe('/');
+  });
+
+  it('sign in: wrong email or password or user not found', async () => {
+    const res = await request.agent(server)
+      .post('/session')
+      .send({
+        form: {
+          email: faker.internet.email(),
+          password: faker.internet.password(),
+        },
+      });
+    expect(res).toHaveHTTPStatus(200);
+  });
+
+  it('sign in: success', async () => {
+    const email = faker.internet.email();
+    const password = faker.internet.password();
+    await request.agent(server)
+      .post('/users')
+      .send({
+        form: {
+          email,
+          firstName: faker.name.firstName(),
+          lastName: faker.name.lastName(),
+          password,
+        },
+      });
+    const res = await request.agent(server)
+      .post('/session')
+      .send({
+        form: {
+          email,
+          password,
+        },
+      });
+    expect(res).toHaveHTTPStatus(302);
+    expect(res.headers.location).toBe('/');
+  });
+
+  it('sign out', async () => {
+    const res = await request.agent(server).delete('/session');
+    expect(res).toHaveHTTPStatus(302);
+    expect(res.headers.location).toBe('/');
+  });
+
+  it('can not view other profile', async () => {
+    const res = await request.agent(server)
+      .get('/users/1');
+    expect(res).toHaveHTTPStatus(302);
+    expect(res.headers.location).toBe('/session/new');
+  });
+
+  it('can not update other profile', async () => {
+    const res = await request.agent(server)
+      .patch('/users/1');
+    expect(res).toHaveHTTPStatus(302);
+    expect(res.headers.location).toBe('/session/new');
+  });
+
+  it('can not edit other profile', async () => {
+    const res = await request.agent(server)
+      .get('/users/1/edit');
+    expect(res).toHaveHTTPStatus(302);
+    expect(res.headers.location).toBe('/session/new');
+  });
+
+  it('can not delete other profile', async () => {
+    const res = await request.agent(server)
+      .delete('/users/1');
+    expect(res).toHaveHTTPStatus(302);
+    expect(res.headers.location).toBe('/session/new');
   });
 
   afterEach((done) => {
