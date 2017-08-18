f6852ac996efdc62f0c5a50886f32d84668c346a
Anton Shvab <shwab.ay@gmail.com>
2017-08-19 01:06:49 +0300

    Create models associations

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,29 @@
 import getUser from './User';
+import getTaskStatus from './TaskStatus';
+import getTag from './Tag';
+import getTask from './Task';
+import getTaskTag from './TaskTag';
 
-export default connect => ({
-  User: getUser(connect),
-});
+export default (connect) => {
+  const models = {
+    User: getUser(connect),
+    Task: getTask(connect),
+    Tag: getTag(connect),
+    TaskTag: getTaskTag(connect),
+    TaskStatus: getTaskStatus(connect),
+  };
+
+  models.User.hasMany(models.Task, { foreignKey: 'creatorId', as: 'creator' });
+  models.Task.belongsTo(models.User, { as: 'creator' });
+
+  models.User.hasMany(models.Task, { foreignKey: 'assignedToId', as: 'assignedTo' });
+  models.Task.belongsTo(models.User, { as: 'assignedTo' });
+
+  models.TaskStatus.hasMany(models.Task, { foreignKey: 'statusId', as: 'status' });
+  models.Task.belongsTo(models.TaskStatus, { as: 'status' });
+
+  models.Tag.belongsToMany(models.Task, { through: models.TaskTag });
+  models.Task.belongsToMany(models.Tag, { through: models.TaskTag });
+
+  return models;
+};
