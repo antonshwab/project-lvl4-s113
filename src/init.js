3149a2915a54103e01765755ece00f06eaf4b9a3
Anton Shvab <shwab.ay@gmail.com>
2017-08-19 02:06:55 +0300

    Fix

--- src/init.js
+++ src/init.js
@@ -3,7 +3,13 @@ import getModels from './models';
 
 export default async () => {
   const models = getModels(connect);
-  await Promise.all(Object.values(models).map(model => model.sync({ force: true })));
+  // await Promise.all(Object.values(models).map(model => model.sync({ force: true })));
+
+  await models.User.sync({ force: true });
+  await models.Task.sync({ force: true });
+  await models.TaskStatus.sync({ force: true });
+  await models.Tag.sync({ force: true });
+  await models.TaskTag.sync({ force: true });
   await models.TaskStatus.bulkCreate([
     { name: 'New' },
     { name: 'Working' },
