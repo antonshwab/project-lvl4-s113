543786d8dc3ad636ee6013d80a84f6904afa6fe9
Anton Shvab <shwab.ay@gmail.com>
2017-08-19 01:54:47 +0300

    Fix

--- src/models/index.js
+++ src/models/index.js
@@ -15,12 +15,13 @@ export default (connect) => {
 
   models.User.hasMany(models.Task, { foreignKey: 'creatorId', as: 'creator' });
   models.User.hasMany(models.Task, { foreignKey: 'assignedToId', as: 'assignedTo' });
+
   models.Task.belongsTo(models.User, { as: 'creator' });
   models.Task.belongsTo(models.User, { as: 'assignedTo' });
-
-  models.Task.belongsToMany(models.Tag, { through: models.TaskTag });
-  models.Tag.belongsToMany(models.Task, { through: models.TaskTag });
   models.Task.belongsTo(models.TaskStatus, { as: 'status' });
+
+  models.Task.belongsToMany(models.Tag, { through: 'TaskTag' });
+  models.Tag.belongsToMany(models.Task, { through: 'TaskTag' });
   models.TaskStatus.hasMany(models.Task, { foreignKey: 'statusId', as: 'status' });
 
   return models;
