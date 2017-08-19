982e468087b784d64a659bd778d3febf558621a7
Anton Shvab <shwab.ay@gmail.com>
2017-08-19 15:28:50 +0300

    Add redirect controller

--- src/controllers/redirects.js
+++ src/controllers/redirects.js
@@ -3,5 +3,9 @@ export default (router) => {
     .get('403', '/403', async (ctx) => {
       ctx.status = 403;
       ctx.render('redirects/403');
+    })
+    .get('404', '/404', async (ctx) => {
+      ctx.status = 404;
+      ctx.render('redirects/404');
     });
 };
